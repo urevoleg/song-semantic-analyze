68296c7f5408ab357b6dec523eaae53a27650c61
urevoleg <urevolegg@gmail.com>
2022-09-25 01:07:17 +0300

    fix

--- app/analyze.py
+++ app/analyze.py
@@ -1,3 +1,4 @@
+import os
 import re
 import datetime as dt
 from pprint import pprint
@@ -12,6 +13,7 @@ import seaborn as sns
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD, LatentDirichletAllocation, NMF
+from sklearn.manifold import TSNE
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 
@@ -50,6 +52,7 @@ def get_text(corpus: List[str]=corpus) -> str:
 class TextNormalizer(BaseEstimator, TransformerMixin):
 
     def __init__(self, language: str='russian'):
+        self.language = language
         self.stopwords = set(stopwords.words(language))
         self.lemmantizer = WordNetLemmatizer()
         self.stemmer = SnowballStemmer(language)
@@ -95,20 +98,26 @@ class TopicModels():
         if estimator == 'LSA':
             self.estimator = TruncatedSVD(n_components=self.n_topics, random_state=88)
         if estimator == 'LDA':
-            self.estimator = LatentDirichletAllocation(n_components=self.n_topics, random_state=88)
+            self.estimator = LatentDirichletAllocation(n_components=self.n_topics, random_state=88,
+                                                        learning_method="online",
+                                                        learning_offset=50.0)
         if estimator == 'NMF':
             self.estimator = NMF(n_components=self.n_topics, random_state=88)
 
         if vect == 'count':
-            self.vectorizer = CountVectorizer(preprocessor=None, lowercase=False)
+            self.vectorizer = CountVectorizer(preprocessor=None, lowercase=True,
+                                              max_df=0.25, min_df=3, max_features=25,
+                                              stop_words='english')
         if vect == 'tfidf':
-            self.vectorizer = TfidfVectorizer(lowercase=False, ngram_range=(1, 3),
-                                              max_df=0.25, min_df=0, max_features=500)
+            self.vectorizer = TfidfVectorizer(lowercase=True, ngram_range=(1, 3),
+                                              max_df=0.25, min_df=3, max_features=25,
+                                              stop_words='english')
 
         self.model = Pipeline([
-            ("norm", TextNormalizer(language=self.language)),
+            #("norm", TextNormalizer(language=self.language)),
             ("vect", self.vectorizer),
-            ("model", self.estimator)
+            ("model", self.estimator),
+            ("reduction", TSNE(n_components=2, n_jobs=os.cpu_count()//2, random_state=88))
         ], verbose=1)
 
     def fit_transform(self, corpus):
@@ -117,7 +126,7 @@ class TopicModels():
 
     def get_topics(self, n_lexemes=5):
         vectorizer = self.model.named_steps['vect']
-        model = self.model.steps[-1][1]
+        model = self.model.named_steps['model']
         names = vectorizer.get_feature_names_out()
         topics = {}
 
@@ -130,8 +139,12 @@ class TopicModels():
 
 class SongCorpusReader():
     def __init__(self,
-                 list_of_song_id=set([1805, 286528, 367965, 256283, 105489]),
-                 list_of_artist_id=set([43020])):
+                 list_of_song_id=set([1805]),
+                 list_of_artist_id=set([12968, 55403, 48537, 9941, 39218])):
+        """
+        :param list_of_song_id:
+        :param list_of_artist_id: (12968 - Diana Krall, 55403 - Within Temptation, 48537 - The Beatles)
+        """
         self.list_of_song_id = list_of_song_id
         self.list_of_artist_id = list_of_artist_id
 
@@ -144,21 +157,32 @@ class SongCorpusReader():
             yield row.text
 
 
+def plot_top_words(model, feature_names, n_top_words, title, n_topics):
+    fig, axes = plt.subplots(ncols=n_topics, figsize=(30, 15), sharex=True)
+    axes = axes.flatten()
+    for topic_idx, topic in enumerate(model.components_):
+        top_features_ind = topic.argsort()[: -n_top_words - 1 : -1]
+        top_features = [feature_names[i] for i in top_features_ind]
+        weights = topic[top_features_ind]
+
+        ax = axes[topic_idx]
+        ax.barh(top_features, weights, height=0.7)
+        ax.set_title(f"Topic {topic_idx +1}", fontdict={"fontsize": 30})
+        ax.invert_yaxis()
+        ax.tick_params(axis="both", which="major", labelsize=20)
+        for i in "top right left".split():
+            ax.spines[i].set_visible(False)
+        fig.suptitle(title, fontsize=40)
+
+    plt.subplots_adjust(top=0.90, bottom=0.05, wspace=0.90, hspace=0.3)
+    plt.show()
+
+
 if __name__ == '__main__':
     logger.info(dt.datetime.now())
-    n_topics = 2
+    n_topics = 3
     most_freq_artists = db.session.query(MostFreqArtist.artist_id)
-    corpus = SongCorpusReader().get_text()
-
-    """
-    normalizer = TextNormalizer()
-    normalized_corpus = [*normalizer.fit_transform(corpus)]
-    print(normalized_corpus)
-
-    vectorizer = CountVectorizer(preprocessor=None, lowercase=None)
-    vectorized_corpus = vectorizer.fit_transform(normalized_corpus)
-    print(vectorized_corpus)
-    print(vectorizer.get_feature_names_out())"""
+    corpus = SongCorpusReader(list_of_artist_id=[row.artist_id for row in most_freq_artists]).get_text()
 
     lsa = TopicModels(n_topics=n_topics, estimator='LSA', language='english', vect='tfidf')
     lsa.fit_transform(corpus)
@@ -175,9 +199,12 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots(figsize=(9, 6))
     sns.despine(ax=ax)
-    ax.plot(lsa.trancated_corpus[:, 0], lsa.trancated_corpus[:, 1], marker='s', ms=5, ls='none', alpha=0.125)
+    ax.plot(lsa.trancated_corpus[:, 0], lsa.trancated_corpus[:, 1], marker='s', ms=5, ls='none', alpha=0.25)
     ax.grid(axis="both", lw=0.25, color='grey')
     plt.show()
+
+    tf_feature_names = lsa.model.named_steps['vect'].get_feature_names_out()
+    plot_top_words(lsa.model.named_steps['model'], tf_feature_names, 10, "Topics in LSA model", n_topics)
 
 
 
