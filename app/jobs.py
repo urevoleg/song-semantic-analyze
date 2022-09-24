cfb7a89d35a4d197f99c598bd493f55114995132
urevoleg <urevolegg@gmail.com>
2022-09-24 13:41:42 +0300

    load songs

--- app/jobs.py
+++ app/jobs.py
@@ -3,14 +3,15 @@ import time
 import re
 import os
 
-from tqdm.auto import tqdm
-
 import gzip
 
 from typing import List, Dict
 
+from sqlalchemy import func
+
 from app import db, app, logger
 from app.models import Song, Text
+from app.sources import SourceSong
 from config import Config
 
 
@@ -31,13 +32,15 @@ def read_song_text(path: str) -> Dict:
         }
 
 
-def init_load():
+def init_load_texts():
     logger.info(dt.datetime.now())
     batch = 0
     total = 0
+    successed_ids = set([])
     for file_path in get_song_text():
         row = read_song_text(path=file_path)
-        if db.session.query(True).filter(Text.song_id == row.get("song_id")).scalar() is None:
+        if row.get("song_id") not in successed_ids:
+            successed_ids.add(row.get("song_id"))
             db.session.add(Text(**row))
             batch += 1
             if batch == 5000:
@@ -49,8 +52,33 @@ def init_load():
     logger.info("Done")
 
 
+def init_load_songs():
+    logger.info(dt.datetime.now())
+    batch = 5000
+    total = db.session.query(func.count(SourceSong.id)).scalar()
+    cnt = 0
+    for n in range(0, total, batch):
+        raw = db.session.query(SourceSong.id.label("song_id"),
+                                 SourceSong.lyricsnet_id,
+                                 SourceSong.album_id,
+                                 SourceSong.artist_id,
+                                 SourceSong.title).offset(n).limit(batch)
+        obj = [{
+            "song_id": row.song_id,
+            "lyricsnet_id": row.lyricsnet_id,
+            "album_id": row.album_id,
+            "artist_id": row.artist_id,
+            "title": row.title
+        } for row in raw]
+        db.session.bulk_insert_mappings(Song, obj)
+        db.session.commit()
+        cnt+=batch
+        logger.debug(f"New 5000 commited! (Total: {cnt})")
+    logger.info("Done")
+
+
 if __name__ == '__main__':
-    init_load()
+    init_load_songs()
 
 
 
